bda27a1cd4f6837ec16e82c34ecb1a94cf2051c8
shubham <gavadeshubham2004@gmail.com>
2025-04-01 22:53:44 +0530

    new file:   components.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/globals.css 	modified:   src/app/page.js 	new file:   src/components/ui/button.jsx 	new file:   src/lib/utils.js 	modified:   tailwind.config.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100 text-gray-900">
-      <h1 className="text-6xl font-bold underline">Hello world!</h1>
+    <div>
+      <Button>Click me</Button>
     </div>
-  );
+  )
 }
