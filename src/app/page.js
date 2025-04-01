d7029f6e7219285c4f2dd50f7faa338cc5863f53
shubham <gavadeshubham2004@gmail.com>
2025-04-02 00:50:37 +0530

    modified:   package-lock.json 	modified:   package.json 	new file:   src/app/_components/Hero.jsx 	modified:   src/app/page.js 	new file:   src/components/ui/container-scroll-animation.jsx 	modified:   tailwind.config.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
 import { Button } from "@/components/ui/button";
 import Head from "next/head";
 import Header from "./_components/Header";
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
     <>
         <Header />
-        
+        <Hero/>
     </>
   );
 }
