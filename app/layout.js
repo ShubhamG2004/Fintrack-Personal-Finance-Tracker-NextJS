10eef797dbb6b6d201f6b3701ef6e9dc4493ebbe
shubham <gavadeshubham2004@gmail.com>
2025-05-08 13:17:31 +0530

    modified:   app/layout.js

--- app/layout.js
+++ app/layout.js
@@ -19,14 +19,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <ClerkProvider
-         publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-        >
-          {children}
-        </ClerkProvider>
-      </body>
-    </html>
-  );
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
+  )
 }
